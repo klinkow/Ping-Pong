6112ce434e6ed7b323b160549524b1ae9c66bc52
Bob <klinkow@gmail.com>
2016-10-14 20:32:18 -0700

    add functional html and css as well as a blueprint for the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
+var inputtedNumber = #numberInput.val;
+
+
+
 $(document).ready(function() {
+  $(button).click(function(event)) {
+    var number = inputtedNumber;
+    var array = [inputtedNumber];
+    for(i=1, i<inputtedNumber, i+=1) {
+      number -= 1;
+      array.append(number);
+    }
+
+    array.foreach {
+      if(val%3=0 && val%5~=0) {
+        val="ping";
+      } else if(val%5=0 && val%3~=0) {
+        val="pong";
+      } else if(val%15) {
+        val="pingpong";
+      }
+    }
+
+    array.forforeach {
+      $(#display)append("<li>"+ val +"</li>")
+    }
+  }
 
 
 })
