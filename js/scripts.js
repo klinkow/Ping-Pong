9a523abeb4b927e03e12613184c04e8141c8e79a
Bob <klinkow@gmail.com>
2016-10-16 14:47:57 -0700

    clean up the javascript, though it is still not functional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,34 @@
-var inputtedNumber = #numberInput.val;
+var inputtedNumber = $("#numberInput").val;
 
 
 
 $(document).ready(function() {
-  $(button).click(function(event)) {
+  $("#btn").click(function(event) {
+    event.preventDefault();
     var number = inputtedNumber;
-    var array = [inputtedNumber];
-    for(i=1, i<inputtedNumber, i+=1) {
+    var listOfNumbers = [inputtedNumber];
+
+    console.log(1)
+
+    for(var i=1; i < inputtedNumber; i+=1) {
       number -= 1;
-      array.append(number);
-    }
+      listOfNumbers.add(number);
+    };
 
-    array.foreach {
-      if(val%3=0 && val%5~=0) {
-        val="ping";
-      } else if(val%5=0 && val%3~=0) {
-        val="pong";
-      } else if(val%15) {
-        val="pingpong";
+    listOfNumbers.foreach(function(listOfNumber) {
+      if ((listOfNumber%3==0) && (listOfNumber%5!=0)) {
+        listOfNumber="ping";
+      } else if (listOfNumber%5==0 && listOfNumber%3!=0) {
+        listOfNumber="pong";
+      } else if (listOfNumber%15==0) {
+        listOfNumber="pingpong";
       }
-    }
+    });
 
-    array.forforeach {
-      $(#display)append("<li>"+ val +"</li>")
-    }
-  }
+    listOfNumbers.foreach(function(listOfNumber) {
+      $("#display").append("<li>" + listOfNumber + "</li>");
+    });
+  });
 
 
-})
+});
